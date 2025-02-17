cb4d99a44de1f0c6cc84199f0a1162d8859e2f22
vklight <vaibhav.kumar@lightfinance.com>
2025-02-17 07:15:41 +0530

    Model Pusher Completed

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -74,6 +74,8 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr)
             save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_obj)
 
+            save_object('final_model/preprocessor.pkl',preprocessor_obj)
+
             # preparing artifacts
             data_transformation_artifact=DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
